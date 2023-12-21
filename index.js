9cb469f66133f35e82d9f863088ac3d5eed7c5ed
Adrien Glon <henriotadrien2310@gmail.com>
2023-12-21 22:24:40 +0100

    correction

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-const mousemove = document.querySelector(".mousemove");
-const mousemove2 = document.querySelector(".mousemove2");
-const gros = document.querySelector(".gros");
+//**********************  CORRECTION  **********************//
+const mouses = document.querySelectorAll(".mouse");
 
 window.addEventListener("mousemove", (e) => {
-  mousemove.style.left = e.pageX + "px";
-  mousemove.style.top = e.pageY + "px";
-
-  mousemove2.style.left = e.pageX + "px";
-  mousemove2.style.top = e.pageY + "px";
-
-  gros.style.left = e.pageX + "px";
-  gros.style.top = e.pageY + "px";
+  mouses.forEach((mouse) => {
+    mouse.style.top = e.y + "px";
+    mouse.style.left = e.x + "px";
+  });
 });
 
-// window.addEventListener("mousemove2", (e) => {
+// window.addEventListener("mousemove", (e) => {
+//   cursor.style.top = e.y + "px";
+//   cursor.style.left = e.x + "px";
+
+//   mouse1.style.top = e.y + "px";
+//   mouse1.style.left = e.x + "px";
+
+//   mouse2.style.top = e.y + "px";
+//   mouse2.style.left = e.x + "px";
+// })
+
+//************************************************************//
+
+// const mousemove = document.querySelector(".mousemove");
+// const mousemove2 = document.querySelector(".mousemove2");
+// const gros = document.querySelector(".gros");
+
+// window.addEventListener("mousemove", (e) => {
+//   mousemove.style.left = e.pageX + "px";
+//   mousemove.style.top = e.pageY + "px";
+
 //   mousemove2.style.left = e.pageX + "px";
 //   mousemove2.style.top = e.pageY + "px";
-// });
 
-// window.addEventListener("gros", (e) => {
 //   gros.style.left = e.pageX + "px";
 //   gros.style.top = e.pageY + "px";
 // });
